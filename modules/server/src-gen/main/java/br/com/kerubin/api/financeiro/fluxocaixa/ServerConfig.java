b071c27080a0a05373a584095184646fb450fae2
Márcio Koch <lobokoch@gmail.com>
2019-06-16 13:40:07 -0300

    Ajustado Jackosn datetime format

--- modules/server/src-gen/main/java/br/com/kerubin/api/financeiro/fluxocaixa/ServerConfig.java
+++ modules/server/src-gen/main/java/br/com/kerubin/api/financeiro/fluxocaixa/ServerConfig.java
@@ -8,6 +8,7 @@ WARNING: DO NOT CHANGE THIS CODE BECAUSE THE CHANGES WILL BE LOST IN THE NEXT CO
 
 package br.com.kerubin.api.financeiro.fluxocaixa;
 
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -15,12 +16,40 @@ import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
 import br.com.kerubin.api.messaging.core.DomainEvent;
 
 @Configuration
 public class ServerConfig {
+	
+	private static final String dateFormat = "yyyy-MM-dd";
+    private static final String dateTimeFormat = "yyyy-MM-dd HH:mm:ss";
+ 
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> {
+            builder.simpleDateFormat(dateTimeFormat);
+            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(dateFormat)));
+            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimeFormat)));
+            
+            ObjectMapper objectMapper = new ObjectMapper();
+    		objectMapper.registerModule(new JavaTimeModule());
+    		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    		objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    		
+    		builder.configure(objectMapper);
+        };
+    }
 	
 	// TODO: remover esse cara
 	@Bean
