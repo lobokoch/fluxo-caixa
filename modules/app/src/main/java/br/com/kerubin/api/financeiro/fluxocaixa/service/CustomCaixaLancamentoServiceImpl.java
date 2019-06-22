b91942bc6d316fd2da0fd47b579ae03f06eb0e8e
Márcio Koch <lobokoch@gmail.com>
2019-06-22 07:27:40 -0300

    Validação para somente poder salvar lançamentos se o caixa estiver aberto

--- modules/app/src/main/java/br/com/kerubin/api/financeiro/fluxocaixa/service/CustomCaixaLancamentoServiceImpl.java
+++ modules/app/src/main/java/br/com/kerubin/api/financeiro/fluxocaixa/service/CustomCaixaLancamentoServiceImpl.java
@@ -1,7 +1,9 @@
 package br.com.kerubin.api.financeiro.fluxocaixa.service;
 
 import java.util.Collection;
+import java.util.UUID;
 
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +17,11 @@ import br.com.kerubin.api.financeiro.fluxocaixa.CaixaDiarioSituacao;
 import br.com.kerubin.api.financeiro.fluxocaixa.entity.caixa.CaixaAutoCompleteImpl;
 import br.com.kerubin.api.financeiro.fluxocaixa.entity.caixadiario.CaixaDiarioAutoComplete;
 import br.com.kerubin.api.financeiro.fluxocaixa.entity.caixadiario.CaixaDiarioAutoCompleteImpl;
+import br.com.kerubin.api.financeiro.fluxocaixa.entity.caixadiario.CaixaDiarioEntity;
+import br.com.kerubin.api.financeiro.fluxocaixa.entity.caixadiario.CaixaDiarioRepository;
 import br.com.kerubin.api.financeiro.fluxocaixa.entity.caixadiario.QCaixaDiarioEntity;
+import br.com.kerubin.api.financeiro.fluxocaixa.entity.caixalancamento.CaixaLancamentoEntity;
+import br.com.kerubin.api.financeiro.fluxocaixa.entity.caixalancamento.CaixaLancamentoRepository;
 import br.com.kerubin.api.financeiro.fluxocaixa.entity.caixalancamento.CaixaLancamentoServiceImpl;
 
 @Primary
@@ -24,6 +30,57 @@ public class CustomCaixaLancamentoServiceImpl extends CaixaLancamentoServiceImpl
 	
 	@Autowired
 	private EntityManager em;
+	
+	@Inject
+	private CaixaDiarioRepository caixaDiarioRepository;
+	
+	@Inject
+	private CaixaLancamentoRepository caixaLancamentoRepository;
+	
+	@Override
+	public CaixaLancamentoEntity create(CaixaLancamentoEntity caixaLancamentoEntity) {
+		validateCaixa(caixaLancamentoEntity);
+		
+		return super.create(caixaLancamentoEntity);
+	}
+	
+	@Override
+	public CaixaLancamentoEntity update(UUID id, CaixaLancamentoEntity caixaLancamentoEntity) {
+		validateCaixa(caixaLancamentoEntity);
+		
+		return super.update(id, caixaLancamentoEntity);
+	}
+	
+	@Override
+	public void delete(UUID id) {
+		CaixaLancamentoEntity caixaLancamentoEntity = caixaLancamentoRepository.findById(id).orElse(null);
+		validateCaixa(caixaLancamentoEntity);
+		super.delete(id);
+	}
+	
+
+	private void validateCaixa(CaixaLancamentoEntity caixaLancamentoEntity) {
+		if (caixaLancamentoEntity == null) {
+			throw new IllegalStateException("Lançamento inválido.");
+		}
+		
+		if (caixaLancamentoEntity.getCaixaDiario() == null) {
+			throw new IllegalStateException("Caixa não informado para o lançamento.");
+		}
+		
+		UUID caixaDiarioId = caixaLancamentoEntity.getCaixaDiario().getId();
+		if (caixaDiarioId == null) {
+			throw new IllegalStateException("Identificador do caixa é inválido para o lançamento.");
+		}
+		
+		CaixaDiarioEntity caixaDiario = caixaDiarioRepository.findById(caixaDiarioId).orElse(null);
+		if (caixaDiario == null) {
+			throw new IllegalStateException("Caixa inválido para o lançamento.");
+		}
+		if (!CaixaDiarioSituacao.ABERTO.equals(caixaDiario.getCaixaDiarioSituacao())) {
+			throw new IllegalStateException("O caixa do lançamento deve estar aberto.");
+		}
+	}
 	
 	@SuppressWarnings("unchecked")
 	@Override
