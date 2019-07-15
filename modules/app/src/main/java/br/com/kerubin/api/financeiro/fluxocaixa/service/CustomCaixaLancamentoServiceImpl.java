cdbb83e8fec17021603fece98e71c8af9b07e228
Márcio Koch <lobokoch@gmail.com>
2019-07-15 08:21:48 -0300

    Ajustes devido à descrição do plano de contas.

--- modules/app/src/main/java/br/com/kerubin/api/financeiro/fluxocaixa/service/CustomCaixaLancamentoServiceImpl.java
+++ modules/app/src/main/java/br/com/kerubin/api/financeiro/fluxocaixa/service/CustomCaixaLancamentoServiceImpl.java
@@ -1,17 +1,29 @@
 package br.com.kerubin.api.financeiro.fluxocaixa.service;
 
+import static br.com.kerubin.api.servicecore.util.CoreUtils.isNotEmpty;
+
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import br.com.kerubin.api.financeiro.fluxocaixa.CaixaDiarioSituacao;
@@ -24,16 +36,20 @@ import br.com.kerubin.api.financeiro.fluxocaixa.entity.caixadiario.CaixaDiarioRe
 import br.com.kerubin.api.financeiro.fluxocaixa.entity.caixadiario.QCaixaDiarioEntity;
 import br.com.kerubin.api.financeiro.fluxocaixa.entity.caixalancamento.CaixaLancamento;
 import br.com.kerubin.api.financeiro.fluxocaixa.entity.caixalancamento.CaixaLancamentoEntity;
+import br.com.kerubin.api.financeiro.fluxocaixa.entity.caixalancamento.CaixaLancamentoListFilter;
 import br.com.kerubin.api.financeiro.fluxocaixa.entity.caixalancamento.CaixaLancamentoRepository;
 import br.com.kerubin.api.financeiro.fluxocaixa.entity.caixalancamento.CaixaLancamentoServiceImpl;
 import br.com.kerubin.api.financeiro.fluxocaixa.entity.planoconta.PlanoContaAutoComplete;
 import br.com.kerubin.api.financeiro.fluxocaixa.entity.planoconta.PlanoContaAutoCompleteImpl;
+import br.com.kerubin.api.financeiro.fluxocaixa.entity.planoconta.PlanoContaEntity;
 import br.com.kerubin.api.financeiro.fluxocaixa.entity.planoconta.QPlanoContaEntity;
-import static br.com.kerubin.api.messaging.utils.Utils.*;
 
 @Primary
 @Service
 public class CustomCaixaLancamentoServiceImpl extends CaixaLancamentoServiceImpl {
+	
+	private static final UUID PLANO_CONTA_RECEITAS_ID = UUID.fromString("1ea1d30c-83e2-4f8f-8c39-ee53ef0d79fe");
+	private static final UUID PLANO_CONTA_DESPESA_ID =  UUID.fromString("5cd7d81e-7e69-4c26-bf2f-12ad2e286fc5");
 	
 	@Autowired
 	private EntityManager em;
@@ -44,7 +60,30 @@ public class CustomCaixaLancamentoServiceImpl extends CaixaLancamentoServiceImpl
 	@Inject
 	private CaixaLancamentoRepository caixaLancamentoRepository;
 	
-	@SuppressWarnings("unchecked")
+	///// Begin custom for plano de contas
+	
+	@Transactional(readOnly = true)
+	@Override
+	public Page<CaixaLancamentoEntity> list(CaixaLancamentoListFilter caixaLancamentoListFilter, Pageable pageable) {
+		Page<CaixaLancamentoEntity> result = super.list(caixaLancamentoListFilter, pageable);
+		
+		if (isNotEmpty(result)) {
+			result.forEach(item -> decoratePlanoContas(item.getPlanoContas()));
+		}
+		
+		return result;
+	}
+	
+	@Transactional(readOnly = true)
+	@Override
+	public CaixaLancamentoEntity read(UUID id) {
+		CaixaLancamentoEntity result = super.read(id);
+		if (isNotEmpty(result)) {
+			decoratePlanoContas(result.getPlanoContas());
+		}
+		return result;
+	}
+	
 	@Override
 	public Collection<PlanoContaAutoComplete> planoContaPlanoContasAutoComplete(String query,
 			CaixaLancamento caixaLancamento) {
@@ -68,33 +107,63 @@ public class CustomCaixaLancamentoServiceImpl extends CaixaLancamentoServiceImpl
 		
 		JPAQueryFactory queryDSL = new JPAQueryFactory(em);
 		QPlanoContaEntity qPlanoConta = QPlanoContaEntity.planoContaEntity;
+		QPlanoContaEntity qPlanoContaPai = new QPlanoContaEntity("planoContaPai");
 		
 		BooleanBuilder where = new BooleanBuilder();
 		if (isNotEmpty(query)) {
-			where.and(qPlanoConta.descricao.containsIgnoreCase(query));
+			where.and(qPlanoConta.descricao.containsIgnoreCase(query).or(qPlanoContaPai.descricao.containsIgnoreCase(query)));
 		}
 		
 		where.and(qPlanoConta.ativo.isTrue())
-		.and(qPlanoConta.deleted.isFalse());
+		.and(qPlanoConta.deleted.isFalse().or(qPlanoConta.deleted.isNull()));
 		
 		if (isNotEmpty(tipoPlanoContaFinanceiro)) {
 			where.and(qPlanoConta.tipoFinanceiro.eq(tipoPlanoContaFinanceiro));
 		}
 		
+		StringExpression descricaoConcatenation = emptyIfNull(qPlanoContaPai.codigo)
+				.concat(" - ")
+				.concat(emptyIfNull(qPlanoContaPai.descricao))
+				.concat(" / ")
+				.concat(qPlanoConta.codigo)
+				.concat(" - ")
+				.concat(qPlanoConta.descricao);
 		
-		Collection<? extends PlanoContaAutoComplete> result = queryDSL.select(
-				Projections.bean(PlanoContaAutoCompleteImpl.class, 
-						qPlanoConta.id, 
-						qPlanoConta.codigo, 
-						qPlanoConta.descricao
-						)
-		)
+		JPAQuery<Tuple> query_ = queryDSL.select(
+				qPlanoConta.id, 
+				qPlanoConta.codigo,
+				descricaoConcatenation
+				)
 		.from(qPlanoConta)
+		.leftJoin(qPlanoConta.planoContaPai, qPlanoContaPai).on(qPlanoContaPai.id.eq(qPlanoConta.planoContaPai.id))
 		.where(where)
-		.orderBy(qPlanoConta.codigo.asc())
-		.fetch();
-		 
-		return (Collection<PlanoContaAutoComplete>) result;
+		.orderBy(qPlanoConta.codigo.asc());
+		
+		List<Tuple> tuples = query_.fetch();
+		
+		List<PlanoContaAutoComplete> items = new ArrayList<>();
+		if (tuples != null && !tuples.isEmpty()) {
+			for (Tuple tuple: tuples) {
+				PlanoContaAutoCompleteImpl plano = new PlanoContaAutoCompleteImpl();
+				plano.setId(tuple.get(0, UUID.class));
+				plano.setCodigo(tuple.get(1, String.class));
+				plano.setDescricao(tuple.get(2, String.class));
+				items.add(plano);
+			} // for
+		}
+		
+		if (items.size() > 0) {
+			PlanoContaAutoComplete plano = items.get(0);
+			if (PLANO_CONTA_RECEITAS_ID.equals(plano.getId()) || PLANO_CONTA_DESPESA_ID.equals(plano.getId())) { // "descricao": "- / 1-DESPESAS"
+				((PlanoContaAutoCompleteImpl) plano).setDescricao(plano.getDescricao().replace(" -  / ", ""));
+			}
+		}
+		
+		items = items.stream()
+				.sorted(Comparator.comparingInt(this::codigoToInt))
+				.collect(Collectors.toList());
+		
+		return items;
 		
 	}
 	
@@ -171,6 +240,36 @@ public class CustomCaixaLancamentoServiceImpl extends CaixaLancamentoServiceImpl
 		}
 	}
 	
+	private void decoratePlanoContas(PlanoContaEntity planoContas) {
+		if (isNotEmpty(planoContas)) { // Adjusts the field descricao of plano de contas and plano de contas pai.
+			String descricao = planoContas.getCodigo() + " - " + planoContas.getDescricao();
+			PlanoContaEntity planoContasPai = planoContas.getPlanoContaPai();
+			if (isNotEmpty(planoContasPai)) {
+				descricao = planoContasPai.getCodigo() + " - " + planoContasPai.getDescricao() + " / " + descricao;
+			}
+			planoContas.setDescricao(descricao);
+		} 
+	}
 	
+	private int codigoToInt(Object toOrderObj) {
+		if (toOrderObj != null && toOrderObj instanceof PlanoContaAutoComplete) {
+			PlanoContaAutoComplete toOrder = (PlanoContaAutoComplete) toOrderObj;
+			String codigo = toOrder.getCodigo();
+			if (isNotEmpty(codigo)) {
+				codigo = codigo.replace(".", "");
+				try {
+					return Integer.parseInt(codigo);
+				} catch(Exception e) {
+					return 0;
+				}
+			}
+		}
+		
+		return 0;
+	}
+	
+	private static StringExpression emptyIfNull(StringExpression expression) {
+	    return expression.coalesce("").asString();
+	}
 
 }
